8743b601458100edee4bd056a9cd6e1381d300b6
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-01-03 10:31:32 -0500

    Update radar_node.py

--- sensor/front_radar/src/radar_node.py
+++ sensor/front_radar/src/radar_node.py
@@ -3,9 +3,9 @@ import rospy
 from lidar_lite import Lidar_Lite
 from std_msgs.msg import Float32
 
-print("Initialize front radar...")
+rospy.loginfo("Initialize front radar...")
 lidar = Lidar_Lite(1)
-print("Done Initialization!")
+rospy.loginfo("Done Initialization!")
 
 def radar():
 	pub = rospy.Publisher('/front_radar', Float32, queue_size=1)
