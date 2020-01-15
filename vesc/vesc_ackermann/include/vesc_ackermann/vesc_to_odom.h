d90fbc4f577a2dd96abd12fb3d53a982b71b1f87
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-01-15 11:12:43 -0500

    Update vesc_to_odom.h

--- vesc/vesc_ackermann/include/vesc_ackermann/vesc_to_odom.h
+++ vesc/vesc_ackermann/include/vesc_ackermann/vesc_to_odom.h
@@ -25,8 +25,8 @@ private:
   /** State message does not report servo position, so use the command instead */
   bool use_servo_cmd_;
   // conversion gain and offset
-  double speed_to_erpm_gain_, speed_to_erpm_offset_;
-  double steering_to_servo_gain_, steering_to_servo_offset_;
+  double speed_to_erpm_gain_, erpm_offset_;
+  double steering_to_servo_gain_, servo_offset_;
   double wheelbase_;
   bool publish_tf_;
 
