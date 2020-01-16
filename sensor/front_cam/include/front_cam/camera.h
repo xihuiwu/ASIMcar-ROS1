da50534ac119a440cfb1f25912c52203dd5e835d
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-01-16 11:21:34 -0500

    Add files via upload

--- sensor/front_cam/include/front_cam/camera.h
+++ sensor/front_cam/include/front_cam/camera.h
@@ -5,8 +5,6 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/Image.h>
-#include <std_msgs/Header.h>
-#include <boost/thread/thread.hpp>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -25,6 +23,34 @@
 #include <string.h>
 #include <errno.h>
 
-namespace vesc_ackermann
+namespace camera
+{
 
+class Camera
+{
+public:
+	Camera(ros::NodeHandle nh, ros::NodeHandle private_nh);
+  
+private:
+	string filename; // pipeline definition
+	int width, height;
+
+	VideoCapture cap; // video capture object
+	Mat frame(width, height, cv::CV_8UC3); // frame
+	cuda::GpuMat frame_gpu; // frame on GPU
+
+	double dataK[9], dataD[4];
+	Mat K, D; // camera intrinsic and extrinsic matrices
+	Mat mapx, mapy; // map function 
+	cuda::GpuMat mapx_gpu, mapy_gpu, frame_gpu, undistort_gpu, undistort_gray_gpu; // variables for un-distortation
+	Mat undistort, undistort_gray;
+	
+	std_msgs::Header header;
+	sensor_msgs::ImagePtr color_ptr, gray_ptr; // image messages of colored and gray images
+
+	ros::Publisher color_pub, gray_pub; // publisher for color and gray images
+};
+
+} // namespace camera
+  
 #endif // CAMERA_H_
