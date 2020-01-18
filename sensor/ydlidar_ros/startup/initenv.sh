5d4f9e217a491d72b2ec09988f769688fae8b91b
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-01-17 19:24:23 -0500

    Add files via upload

--- sensor/ydlidar_ros/startup/initenv.sh
+++ sensor/ydlidar_ros/startup/initenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo  'KERNEL=="ttyTHS2", MODE:="0666", GROUP:="dialout",  SYMLINK+="ydlidar"' >/etc/udev/rules.d/ydlidar.rules
+echo  'KERNEL=="ttyTHS2", MODE:="0666", GROUP:="dialout", SYMLINK+="ydlidar"' >/etc/udev/rules.d/99-uart-serial.rules
 
 service udev reload
 sleep 2
