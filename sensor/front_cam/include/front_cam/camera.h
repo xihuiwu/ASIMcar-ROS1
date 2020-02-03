390faef30f5ab29a9ac4a1934461adf083b317fb
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-02-03 13:30:10 -0500

    Update camera.h

--- sensor/front_cam/include/front_cam/camera.h
+++ sensor/front_cam/include/front_cam/camera.h
@@ -5,6 +5,7 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CompressedImage.h>
 #include <std_msgs/Header.h>
 
 #include <opencv2/core.hpp>
@@ -44,12 +45,12 @@ private:
 	cv::Mat K, D; // camera intrinsic and extrinsic matrices
 	cv::Mat mapx, mapy; // map function 
 	cv::cuda::GpuMat mapx_gpu, mapy_gpu, frame_raw_gpu, frame_gpu, undistort_gpu, undistort_gray_gpu; // variables for un-distortation
-	cv::Mat undistort, undistort_gray;
+	cv::Mat undistort, compressed;
 	
 	std_msgs::Header header;
-	sensor_msgs::ImagePtr color_msg, gray_msg; // image messages of colored and gray images
+	sensor_msgs::ImagePtr color_msg, compressed_msg; // image messages of colored and gray images
 
-	ros::Publisher color_pub, gray_pub; // publisher for color and gray images
+	ros::Publisher color_pub, compress_pub; // publisher for color and gray images
 };
 
 } // namespace camera
