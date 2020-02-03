50da83db0d71232fef2d328f15e375ebbd704bd7
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-02-03 17:38:19 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo apt install ros-melodic-gmapping
 sudo apt install ros-melodic-robot-localization
 sudo apt install ros-melodic-gscam
+sudo apt install ros-melodic-ackermann-msgs
 
 git clone https://github.com/ros-perception/vision_opencv.git
 cd sensor
